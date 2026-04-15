e6104d36531bda238e8f3e73c138c24e4fbd1769
Wynand Winterbach <wynand.winterbach@gmail.com>
2026-04-15 13:55:44 +0200

    Revert "autoresearch iter 17: Use str_sub comparison for true, false, null in decode_value"
    This reverts commit d53b04ac6b87714c11d1ecf90130ace1030c16e8.

--- src/wjson.lua
+++ src/wjson.lua
@@ -1048,15 +1048,15 @@ decode_value = function(str, pos, depth, len, b)
   if b == BYTE_LBRACKET then return parse_array(str, pos, depth, len) end
   if b == BYTE_LBRACE then return parse_object(str, pos, depth, len) end
 
-  if b == BYTE_T and str_sub(str, pos, pos + 3) == "true" then
+  if b == BYTE_T and str_byte(str, pos + 1) == 114 and str_byte(str, pos + 2) == 117 and str_byte(str, pos + 3) == 101 then -- true
     return true, pos + 4
   end
 
-  if b == BYTE_F and str_sub(str, pos, pos + 4) == "false" then
+  if b == BYTE_F and str_byte(str, pos + 1) == 97 and str_byte(str, pos + 2) == 108 and str_byte(str, pos + 3) == 115 and str_byte(str, pos + 4) == 101 then -- false
     return false, pos + 5
   end
 
-  if b == BYTE_N and str_sub(str, pos, pos + 3) == "null" then
+  if b == BYTE_N and str_byte(str, pos + 1) == 117 and str_byte(str, pos + 2) == 108 and str_byte(str, pos + 3) == 108 then -- null
     return null, pos + 4
   end
 
